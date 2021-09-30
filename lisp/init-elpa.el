6a21838ab26dd8ff99b9ad09135ee5035cb69e79
zyp2005 <1715260398@qq.com>
2021-10-01 06:19:59 +0800

    2021.9.30 22:19

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -1,7 +1,7 @@
-(setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
-                         ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
-                         ("org" . "http://mirrors.ustc.edu.cn/elpa/org/")))
+(setq package-archives '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+                         ("melpa-stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")
+                         ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
 (setq package-check-signature nil)
 
 (require 'package)
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -7,4 +7,16 @@
 	sml/theme 'respectful)
   (sml/setup))
 
+(use-package emacs
+  :if (display-graphic-p)
+  :config
+  (if *is-windows*
+      (progn
+	(set-face-attribute 'default nil :font "Microsoft Yahei Mono 9")
+	(dolist (charset 'kana han symbol cjk-misc bopomofo)
+	  (set-fontset-font (frame-parameter nil 'font)
+			    charset (font-spec :family "Microsoft Yahei Mono" :size 12))))
+    (set-face-attribute 'default nil :font "SauceCodePro Nerd Font 13")))
+	
+
 (provide 'init-ui)
